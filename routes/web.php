75bbdfca01205d88d7706c7641d1850dc274996d
Jimmy Christiaens <jimmy_chr@hotmail.com>
2025-08-06 12:42:18 +0200

    feat: adding lesson 6

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,24 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            ['id' => 1, 'title' => 'Director', 'salary' => '€ 60000'],
+            ['id' => 2, 'title' => 'Developer', 'salary' => '€ 50000'],
+            ['id' => 3, 'title' => 'Designer', 'salary' => '€ 50000']
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        ['id' => 1, 'title' => 'Director', 'salary' => '€ 60000'],
+        ['id' => 2, 'title' => 'Developer', 'salary' => '€ 50000'],
+        ['id' => 3, 'title' => 'Designer', 'salary' => '€ 50000']
+    ];
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
