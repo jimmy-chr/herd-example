827f676f616a81e5047733a4d0b2e6730f1e368d
Jimmy Christiaens <jimmy_chr@hotmail.com>
2025-08-08 14:28:08 +0200

    feat: adding eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,26 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            ['id' => 1, 'title' => 'Director', 'salary' => '€ 60000'],
-            ['id' => 2, 'title' => 'Developer', 'salary' => '€ 50000'],
-            ['id' => 3, 'title' => 'Designer', 'salary' => '€ 50000']
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404);
-        }
-        return $job;
-    }
-
+    protected $table = "jobs_listings";
+    protected $fillable = ['title', 'salary'];
 }
