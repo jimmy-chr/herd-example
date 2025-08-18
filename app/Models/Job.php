a4197dd878fc067af346dd257886e979f0388b64
Jimmy Christiaens <jimmy_chr@hotmail.com>
2025-08-18 12:03:01 +0200

    adding relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,4 +10,12 @@ class Job extends Model
     use HasFactory;
     protected $table = "jobs_listings";
     protected $fillable = ['title', 'salary'];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+    }
 }
