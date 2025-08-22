3afca4711a8326a6f093be0a1adc602a217b1270
Jimmy Christiaens <jimmy_chr@hotmail.com>
2025-08-22 12:18:33 +0200

    adding authentication

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,108 +2,90 @@
 <html lang="en" class="h-full bg-gray-100">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Herd Example</title>
-  <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Herd Example</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="h-full">
-  <div class="min-h-full">
-    <nav class="bg-gray-800">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="flex h-16 items-center justify-between">
-          <div class="flex items-center">
-            <div class="shrink-0">
-              <img src="https://assets.laracasts.com/images/primary-logo-symbol.svg" alt="Your Company"
-                class="size-8" />
+    <div class="min-h-full">
+        <nav class="bg-gray-800">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="flex h-16 items-center justify-between">
+                    <div class="flex items-center">
+                        <div class="shrink-0">
+                            <img src="https://assets.laracasts.com/images/primary-logo-symbol.svg" alt="Your Company"
+                                class="size-8" />
+                        </div>
+                        <div class="hidden md:block">
+                            <div class="ml-10 flex items-baseline space-x-4">
+                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                                <x-nav-link href="/" :active="request()->is('/')" type="a">Home</x-nav-link>
+                                <x-nav-link href="/jobs" :active="request()->is('jobs')" type="a">Jobs</x-nav-link>
+                                <x-nav-link href="/contact" :active="request()->is('contact')" type="a">Contact</x-nav-link>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hidden md:block">
+                        <div class="ml-4 flex items-center md:ml-6">
+                            @guest
+                                <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
+                                <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+                            @endguest
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline space-x-4">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <x-nav-link href="/" :active="request()->is('/')" type="a">Home</x-nav-link>
-                <x-nav-link href="/jobs" :active="request()->is('jobs')" type="a">Jobs</x-nav-link>
-                <x-nav-link href="/contact" :active="request()->is('contact')" type="a">Contact</x-nav-link>
-              </div>
-            </div>
-          </div>
-          <div class="hidden md:block">
-            <div class="ml-4 flex items-center md:ml-6">
-              <button type="button"
-                class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                <span class="absolute -inset-1.5"></span>
-                <span class="sr-only">View notifications</span>
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon"
-                  aria-hidden="true" class="size-6">
-                  <path
-                    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                    stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-              </button>
-              <!-- Profile dropdown -->
-              <el-dropdown class="relative ml-3">
-                <button
-                  class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-hidden focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-2 focus-visible:ring-offset-gray-800">
-                  <span class="absolute -inset-1.5"></span>
-                  <span class="sr-only">Open user menu</span>
-                  <img src="https://assets.laracasts.com/images/mascot/larydefault.svg" alt=""
-                    class="size-8 rounded-full" />
-                </button>
-              </el-dropdown>
 
-            </div>
-          </div>
-        </div>
-      </div>
+            <el-disclosure id="mobile-menu" hidden class="block md:hidden">
+                <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                    <a href="/" aria-current="page"
+                        class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Home</a>
+                    <a href="/jobs"
+                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Jobs</a>
+                    <a href="/contact"
+                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                </div>
+                <div class="border-t border-gray-700 pt-4 pb-3">
+                    <div class="flex items-center px-5">
+                        <div class="shrink-0">
+                            <img src="https://assets.laracasts.com/images/mascot/larydefault.svg" alt=""
+                                class="size-10 rounded-full" />
+                        </div>
+                        <div class="ml-3">
+                            <div class="text-base/5 font-medium text-white">Larry</div>
+                            <div class="text-sm font-medium text-gray-400">larry@example.com</div>
+                        </div>
+                        <button type="button"
+                            class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                            <span class="absolute -inset-1.5"></span>
+                            <span class="sr-only">View notifications</span>
+                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
+                                data-slot="icon" aria-hidden="true" class="size-6">
+                                <path
+                                    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                                    stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </el-disclosure>
+        </nav>
 
-      <el-disclosure id="mobile-menu" hidden class="block md:hidden">
-        <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-          <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-          <a href="/" aria-current="page"
-            class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Home</a>
-          <a href="/jobs"
-            class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Jobs</a>
-          <a href="/contact"
-            class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-        </div>
-        <div class="border-t border-gray-700 pt-4 pb-3">
-          <div class="flex items-center px-5">
-            <div class="shrink-0">
-              <img src="https://assets.laracasts.com/images/mascot/larydefault.svg" alt=""
-                class="size-10 rounded-full" />
+        <header class="bg-white shadow-sm">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
+                <x-button href="/jobs/create">Create Job</x-button>
             </div>
-            <div class="ml-3">
-              <div class="text-base/5 font-medium text-white">Larry</div>
-              <div class="text-sm font-medium text-gray-400">larry@example.com</div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{ $slot }}
             </div>
-            <button type="button"
-              class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-              <span class="absolute -inset-1.5"></span>
-              <span class="sr-only">View notifications</span>
-              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon"
-                aria-hidden="true" class="size-6">
-                <path
-                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                  stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </el-disclosure>
-    </nav>
-
-    <header class="bg-white shadow-sm">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
-        <x-button href="/jobs/create">Create Job</x-button>
-      </div>
-    </header>
-    <main>
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        {{ $slot }}
-      </div>
-    </main>
-  </div>
+        </main>
+    </div>
 
 </body>
 
