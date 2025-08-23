777dd53a834ae63c5f3a43d19c7be00976e90558
Jimmy Christiaens <jimmy_chr@hotmail.com>
2025-08-23 14:17:19 +0200

    adding authentication

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +16,17 @@ class RegisteredUserController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
+        $attributes = $request->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed '],
+        ]);
+
+        $user = User::create($attributes);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,8 @@
                     <x-form-field>
                         <x-form-label for="email">Email</x-form-label>
                         <div class="mt-2">
-                            <x-form-input id="email" name="email" type="email" required />
+                            <x-form-input id="email" name="email" type="email" value="{{ old('email') }}"
+                                required />
                             <x-form-error name="email" />
                         </div>
                     </x-form-field>
