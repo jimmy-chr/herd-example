40e7a9452de4d4d599b7f94e367a08895e3965aa
Jimmy Christiaens <jimmy_chr@hotmail.com>
2025-09-03 20:35:24 +0200

    introducing a mock job queue

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
+
+Route::get('test', function () {
+    $job = Job::first();
+    TranslateJob::dispatch($job);
+    return 'Done';
+});
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
